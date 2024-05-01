51c4e8868cab031adb0c06c086301682d40d25f3
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-05-01 16:13:09 +0200

    Input validator

--- DesignProblem/Seating/input_validators/validator.py
+++ DesignProblem/Seating/input_validators/validator.py
@@ -1,15 +1,30 @@
+#!/usr/bin/env python3
+
 import sys
-import re
 
 try: 
-    n = int(sys.stdin.readline())
-    assert 100 >= n > 0
-    for _ in range(n):
-        l = sys.stdin.readline()
-        assert re.match("(0|[1-9][0-9]*) ([1-9][0-9]*)", l) != None
-    line = sys.stdin.readline()
-    assert len(line) == 0
-
+    w,h = map(int, sys.stdin.readline().split())
+    assert 300 >= w > 2
+    assert 300 >= h > 2
+    sources = 0
+    sinks = 0
+    free = 0
+    lines = sys.stdin.readlines();
+    assert len(lines) == h
+    for rawLine in lines:
+        line = rawLine.split()
+        assert len(line) == w
+        for char in line:
+            assert char in ["0", "S", "I", "H"]
+            if char == "0":
+                free += 1
+            elif char == "S":
+                sinks += 1
+            elif char == "I":
+                sources += 1
+    assert sources >= 1
+    assert sinks >= 1
+    assert free >= 1
     sys.exit(42)
 except ValueError:
     sys.exit(43)
