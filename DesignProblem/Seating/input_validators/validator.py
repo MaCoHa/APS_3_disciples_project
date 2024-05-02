be784e84db98da8d2cfa86d502509eb2f9b656d7
Philip Kristian Møller Flyvholm <phif@itu.dk>
2024-05-02 10:57:44 +0200

    Added first solution

--- DesignProblem/Seating/input_validators/validator.py
+++ DesignProblem/Seating/input_validators/validator.py
@@ -6,25 +6,38 @@ try:
     w,h = map(int, sys.stdin.readline().split())
     assert 300 >= w >= 2
     assert 300 >= h >= 2
-    sources = 0
-    sinks = 0
+    sources = set()
+    sinks = set()
     free = 0
     lines = sys.stdin.readlines();
     assert len(lines) == h
+    x = 0
     for rawLine in lines:
         line = rawLine.split()
         assert len(line) == w
+        y = 0
         for char in line:
             assert char in ["0", "S", "I", "H"]
             if char == "0":
                 free += 1
             elif char == "S":
-                sinks += 1
+                sinks.add((x, y))
             elif char == "I":
-                sources += 1
-    assert sources >= 1
-    assert sinks >= 1
+                sources.add((x, y))
+            y += 1
+        x += 1
+    assert len(sources) >= 1
+    assert len(sinks) >= 1
     assert free >= 1
+    
+    for (x,y) in sources:
+        assert (x,y) not in sinks
+        for i in range(-1,2):
+            for j in range(-1,2):
+                if i == 0 and j == 0:
+                    continue
+                if x+i >= 0 and x+i < w and y+j >= 0 and y+j < h:
+                    assert (x+i, y+j) not in sinks
     sys.exit(42)
 except ValueError:
     sys.exit(43)
